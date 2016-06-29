9e19e0d2d13dd79e560603cf82d2583ebf772bda
Andrés Argüelles <aarguellesdelgado@gmail.com>
2016-06-29 01:30:47 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,6 @@ from setuptools import setup
 setup(name='Odoo',
       version='9.0',
       description='Odoo 9.0',
-<<<<<<< HEAD
       author='Andres Arguelles',
       author_email='aarguellesdelgado@gmail.com',
-=======
-      author='AndrésArgüelles',
-      author_email='aarguellesdelgado@gmail.com',
-##      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
->>>>>>> ec9e7e1e5f5e1d8e842094b0ab98583509e73729
      )
